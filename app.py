0576272fb7f1efa7977775bf1e4388a309cdafe2
JT.Han <59948448+JThh@users.noreply.github.com>
2021-06-02 10:44:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,13 +18,19 @@ st.write('You selected',faculty)
 
 module = pd.read_csv(PATH)
 
-cohort1 = st.number_input('Select a cohort to compare',min_value=10,max_value=18)
-st.write(cohort1,'selected')
+col1, col2 = st.beta_columns(2)
 
-cohort2 = st.number_input('Select another cohort to compare with the previous one'+cohort1,min_value=cohort1,max_value=18,help='preferably larger than'+cohort1)
-st.write(cohort2,'selected')
+with col1:
+  cohort1 = st.number_input('Select a cohort to compare',min_value=10,max_value=18)
+  st.write(cohort1,'selected')
+
+with col2:
+  cohort2 = st.number_input('Select another cohort to compare with the previous one'+cohort1,min_value=cohort1,max_value=18,help='preferably larger than'+str(cohort1))
+  st.write(cohort2,'selected')
 
 analyzer = CohortAnalyzer(module, cohort1, cohort2)
+
+
 
 
                                             
