d1e34059f6cc7fd7a0b2d04d19f30e2d5ed9d0d3
JT.Han <59948448+JThh@users.noreply.github.com>
2021-06-02 22:34:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ with st.beta_expander("Statistical Analysis"):
 
 with st.beta_expander("Module enrolment difference analysis"):
   st.markdown('Modules are sorted based on their enrolment differences in descending order.')
-  n_mods = st.number_input('Top k modules to show on the graph',min_value=5, max_value=20, value=10)
+  n_mods = st.number_input('Top k modules',min_value=5, max_value=20, value=10, help='shown on the graph in sorted order')
   st.write('Top',n_mods,'modules are to be shown')
   with st.echo():
     analyzer.get_most_different_modules()
