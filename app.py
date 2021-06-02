6ffdf9a70d9df39aba19797b5187247f0a662ea5
JT.Han <59948448+JThh@users.noreply.github.com>
2021-06-02 08:53:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt
 
-from utils import CohortAnalyzer
+#from utils import CohortAnalyzer
 
 st.title('Cohort Analysis of Module Selection Pattern')
