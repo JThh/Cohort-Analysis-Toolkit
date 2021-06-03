4886100fe642225599bdc5d4cfc2b28ebf882ca7
JT.Han <59948448+JThh@users.noreply.github.com>
2021-06-03 13:06:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,9 @@ def loadDataFromDrive(dataLink, fileName):
     fileName: name of the file in frive
   '''
   # Code to read csv file into Colaboratory:
-  #get_ipython().system('pip install -U -q PyDrive')
-  import google.colab
+  from IPython import get_ipython
+  get_ipython().system('pip install -U -q google-colab')
+  #import google.colab
   from pydrive.auth import GoogleAuth
   from pydrive.drive import GoogleDrive
   from google.colab import auth
