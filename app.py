3c108d435399c01c28c5f3fc1d4b611edead5dbd
JT.Han <59948448+JThh@users.noreply.github.com>
2021-06-03 23:29:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from PIL import Image
 from utils import CohortAnalyzer#, ExcelDataReader
 
 ############################### STRICTLY CONFIDENTIAL ################################
-DRIVE_URL_MOD = 'https://drive.google.com/file/d/1VYLamyLKTYs1TejqkOX1QeNxVeRehE3t/view?usp=sharing'
+DRIVE_URL_MOD = 'https://drive.google.com/file/d/1--KTK4vU5cA1akMGcIsHBmfo3FVfJSdp/view?usp=sharing'
 DRIVE_URL_STU = 'https://drive.google.com/file/d/1aoszzf2FtJD_ijv7tCmLFap0CEdaRcrL/view?usp=sharing'
 PATH_MOD = 'https://drive.google.com/uc?export=download&id=' + DRIVE_URL_MOD.split('/')[-2]
 PATH_STU = 'https://drive.google.com/uc?export=download&id=' + DRIVE_URL_STU.split('/')[-2]
