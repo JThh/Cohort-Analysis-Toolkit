a2af0e76dde72c7fbde1a3065c5c63b4cf60fff0
JT Han <jiatong.han@u.nus.edu>
2021-06-14 10:17:57 +0800

    Modified app.py

--- app.py
+++ app.py
@@ -310,12 +310,12 @@ with st.beta_expander("Principal component analysis"):
 
 
 with st.beta_expander("Attribute percentage analysis"):
-    stu_attr_list = analyzer.academic_plans
+    stu_have_plan, stu_attr_list = analyzer.academic_plans
 
     col1, col2 = st.beta_columns(2)
 
     with col1:
-        if stu_attr_list:
+        if stu_have_plan:
             stu_attr = st.selectbox(
                 "Select a student academic plan to explore", stu_attr_list, index=1
             )
